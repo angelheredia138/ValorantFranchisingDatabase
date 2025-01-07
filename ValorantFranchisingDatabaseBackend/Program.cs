720dacb4ac16fab0bf53325d868308cbec5856b5
Angel Heredia <herediafangel@gmail.com>
2025-01-06 18:28:36 -0700

    init commit

---
+++ ValorantFranchisingDatabaseBackend/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient("ValorantApi", client =>
+{
+    client.BaseAddress = new Uri("https://vlr-franchising-api-eze5a7bxc3ehh4ga.canadacentral-01.azurewebsites.net/api");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.Run();
+
